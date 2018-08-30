3e91c1d591125a65a17faa56258a407517d63858
Fernndo Viton <fcode@viton.org>
2018-08-30 12:30:12 -0700

    add device connectivity sample

---
+++ device-connectivity/program.cs
@@ -0,0 +1,140 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.DigitalTwins.Samples.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Azure.DigitalTwins.Samples
+{
+    class Program
+    {
+        private static Random rnd = new Random();
+        public static Device DeviceInfo { get; set; }
+        public static IConfiguration Configuration { get; set; }
+        static void Main(string[] args)
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.dev.json", optional: true)
+                .Build();
+
+            var hardwareId = GetMacAddress();
+
+            Console.WriteLine($"Your hardware ID is: {hardwareId}");
+
+            var topologyClient = new TopologyClient(Configuration["ManagementApiUrl"], Configuration["SasToken"]);
+            DeviceInfo = topologyClient.GetDeviceForHardwareId(hardwareId).Result;
+            if (DeviceInfo == null)
+            {
+                Console.WriteLine("ERROR: Could not retrieve device information.");
+                return;
+            }
+
+            try
+            {
+                DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(DeviceInfo.ConnectionString);
+
+                if (deviceClient == null)
+                {
+                    Console.WriteLine("Failed to create DeviceClient!");
+                }
+                else
+                {
+                    SendEvent(deviceClient).Wait();
+                }
+
+                Console.WriteLine("Exited!\n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in sample: {0}", ex.Message);
+            }
+        }
+
+        static Func<string> CreateGetRandomSensorReading(string sensorDataType)
+        {
+            switch (sensorDataType)
+            {
+                default:
+                    throw new Exception($"Unsupported configuration: SensorDataType, '{sensorDataType}'");
+                case "Motion":
+                    return () => rnd.Next(0, 2) == 0 ? "false" : "true";
+                case "Temperature":
+                    return () => rnd.Next(70, 100).ToString(CultureInfo.InvariantCulture);
+                case "CarbonDioxide":
+                    return () => rnd.Next(950, 1300).ToString(CultureInfo.InvariantCulture);
+            }
+        }
+
+        static async Task SendEvent(DeviceClient deviceClient)
+        {
+            var serializer = new DataContractJsonSerializer(typeof(TelemetryMessage));
+
+            var sensorDataTypes = Configuration["SensorDataTypes"].Split(',');
+
+            while (true)
+            {
+                foreach (var sensorDataType in sensorDataTypes)
+                {
+                    var getRandomSensorReading = CreateGetRandomSensorReading(sensorDataType);
+
+                    var sensor = DeviceInfo.Sensors.FirstOrDefault(x => (x.DataType == sensorDataType));
+                    if (sensor == null)
+                    {
+                        throw new Exception($"No preconfigured Sensor for DataType '{sensorDataType}' found.");
+                    }
+
+                    var telemetryMessage = new TelemetryMessage()
+                    {
+                        SensorId=sensor.Id,
+                        SensorReading=getRandomSensorReading(),
+                        EventTimestamp=DateTime.UtcNow.ToString("o"),
+                        SensorType=sensor.Type,
+                        SensorDataType=sensor.DataType
+                    };
+
+                    using (var stream = new MemoryStream())
+                    {
+                        serializer.WriteObject(stream, telemetryMessage);
+                        var binaryMessage = stream.ToArray();
+                        Message eventMessage = new Message(binaryMessage);
+                        eventMessage.Properties.Add("Sensor", "");
+                        eventMessage.Properties.Add("MessageVersion", "1.0");
+                        eventMessage.Properties.Add("x-ms-flighting-udf-execution-manually-enabled", "true");
+                        Console.WriteLine($"\t{DateTime.UtcNow.ToLocalTime()}> Sending message: {Encoding.ASCII.GetString(binaryMessage)}");
+
+                        await deviceClient.SendEventAsync(eventMessage).ConfigureAwait(false);
+                    }
+                }
+
+                Thread.Sleep(int.Parse(Configuration["MessageIntervalInMilliSeconds"]));
+            }
+        }
+
+        private static string GetMacAddress()
+        {
+            foreach(var nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                var physAddress = nic.GetPhysicalAddress().ToString();
+                if (physAddress.Length > 0)
+                {
+                    return physAddress;
+                }
+            }
+
+            throw new Exception("No hardware address found.");
+        }
+    }
+}
---
+++ device-connectivity/readme.md
@@ -0,0 +1,48 @@
+# Digital Twins Device Connectivity Sample
+
+This sample provides a small framework to connect a device to Digital Twins providing
+sample sensory data.
+
+## Configuring the app
+
+Edit [appsettings.json](./appsettings.json) to fill in the following values:
+* `ManagementApiUrl`
+* `SasToken`
+
+>Note: You have already generated a SAS Token from a configured Key on the Keystore API. To generate a valid token, you will need to
+know your device's MAC address. Remove all columns (:) from the address and capitalize the value. E.g.: If your MAC address is `18:36:ba:0c:85:13`, the expected value would be `1836BA0C8513`. Please ensure this value is also reflected on the `HardwareId` property of your Topology Device.  If you need to override the hardwareId you can simply edit Program.cs and manually set the hardwareId.
+
+Examples of configuration
+```
+"ManagementApiUrl": "https://name.westcentralus.azuresmartspaces.net/management/","SasToken": "SharedAccessSignature id=1836BA0C8519&se=31556995200&kv=1&sig=nDBUdQcEXkaRgx3Jy3ntwEJ08uP9KxkjoKR2Wa7lCfs%3D",
+```  
+
+## Building the app
+
+* [Install .NET Core SDK](https://www.microsoft.com/net/core) on your execution platform
+* Run `dotnet restore`
+* Run `dotnet build`
+
+## Executing the app
+
+* Run `dotnet run`
+
+>Note: Here is how you can run this app on a Raspberry Pi
+## Running your App on a Raspberry Pi
+
+### Building
+
+Choose `win-arm` or `linux-arm` for your platform:
+* Run `dotnet publish -r <platform>`
+
+Under ./bin/Debug/netcoreapp2.0/<runtime identifier>/publish or .\bin\Debug\netcoreapp2.0\<runtime identifier>\publish you will see the whole self contained app that you need to copy to your Raspberry Pi.
+
+### Windows 10 IoT
+
+* Run on device by invoking the executable
+
+### Linux
+
+* Install [Linux](https://www.raspberrypi.org/downloads/) on your Pi.
+* Install the [platform dependencies from your distro's package manager](https://github.com/dotnet/core/blob/master/Documentation/prereqs.md) for .NET Core.
+* `chmod 755 ./device-connectivity`
