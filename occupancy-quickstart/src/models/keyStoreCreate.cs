e88aac37d414bdeaa90b1db614af7ce931acd86d
Fernndo Viton <fcode@viton.org>
2018-09-20 10:52:55 -0700

    in progress

---
+++ occupancy-quickstart/src/models/keyStoreCreate.cs
@@ -0,0 +1,8 @@
+namespace Microsoft.Azure.DigitalTwins.Samples.Models
+{
+    public class KeyStoreCreate
+    {
+        public string Name { get; set; }
+        public string SpaceId { get; set; }
+    }
+}
