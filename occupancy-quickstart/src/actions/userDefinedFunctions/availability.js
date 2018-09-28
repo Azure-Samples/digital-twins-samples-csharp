3edd4fb1286fa7c82598b17d7dad1502c5fa9f14
Alina Stanciu <alinast@microsoft.com>
2018-09-28 12:53:09 -0700

    add latest UDF

--- occupancy-quickstart/src/actions/userDefinedFunctions/availability.js
+++ occupancy-quickstart/src/actions/userDefinedFunctions/availability.js
@@ -1,21 +1,21 @@
-var CarbonDioxideType = "CarbonDioxide";
+var carbonDioxideType = "CarbonDioxide";
 var motionType = "Motion";
-var spaceAirQuality = "AvailableAndFresh";
+var spaceAvailFresh = "AvailableAndFresh";
+var carbonDioxideThreshold = 1000.0;
 
 function process(telemetry, executionContext) {
   
-    sendNotification(telemetry.SensorId, "CarbonDioxide", JSON.stringify("hello world")); 
-
-    // Log SensorId and Message
-    log(`Sensor ID: ${telemetry.SensorId}. `);
-    log(`Sensor value: ${JSON.stringify(telemetry.Message)}.`);
-
-    var sensor = getSensorMetadata(telemetry.SensorId);
-
-    // Retrieve the sensor value
-    var parseReading = JSON.parse(telemetry.Message);
-
     try {
+        // Log SensorId and Message
+        log(`Sensor ID: ${telemetry.SensorId}. `);
+        log(`Sensor value: ${JSON.stringify(telemetry.Message)}.`);
+
+        // Get sensor metadata
+        var sensor = getSensorMetadata(telemetry.SensorId);
+
+        // Retrieve the sensor reading
+        var parseReading = JSON.parse(telemetry.Message);
+
         // Set the sensor reading as the current value for the sensor.
         setSensorValue(telemetry.SensorId, sensor.DataType, parseReading.SensorValue);
 
@@ -24,11 +24,10 @@ function process(telemetry, executionContext) {
 
         // Get children sensors from the same space
         var otherSensors = parentSpace.ChildSensors();
-        var carbonDioxideThreshold = 1000.0;
 
-        // Retrieve C02, and motion sensors
-        var CarbonDioxideSensor = otherSensors.find(function(element) {
-            return element.DataType === CarbonDioxideType;
+        // Retrieve carbonDioxide, and motion sensors
+        var carbonDioxideSensor = otherSensors.find(function(element) {
+            return element.DataType === carbonDioxideType;
         });
 
         var motionSensor = otherSensors.find(function(element) {
@@ -36,29 +35,33 @@ function process(telemetry, executionContext) {
         });
 
         // get latest values for above sensors
-        var motionVal = motionSensor.Value().Value;
-        var presence = !!motionVal && motionVal.toLowerCase() === "true";
-        var CarbonDioxide = getFloatValue(CarbonDioxideSensor.Value().Value);
-
-        // Return if no motion or C02 found
-        if(motionVal === null || CarbonDioxide === null) {
+        var motionValue = motionSensor.Value().Value;
+        var presence = !!motionValue && motionValue.toLowerCase() === "true";
+        var carbonDioxideValue = getFloatValue(carbonDioxideSensor.Value().Value);
+        
+        // Return if no motion or carbonDioxide found return
+        if(carbonDioxideValue === null || motionValue === null) {
+            sendNotification(telemetry.SensorId, "Sensor", "Error: Carbon dioxide or motion are null, returning");
             return;
         }
+        
+        var availableFresh = "Room is available and air quality is good";
+        var noAvailableOrFresh = "Room not available or air quality is poor";
 
-        // If C02 greater than threshold and presence in the room => log, notify and set parent space computed value 
-        if(CarbonDioxide < carbonDioxideThreshold && !presence) {
-            log(`Room is available and air quality is good. Carbon Dioxide: ${CarbonDioxideSensor.Value().Value}. Presence: ${presence}.`);
-            setSpaceValue(parentSpace.Id, spaceAirQuality, "Room is available and air quality is good.");
+        // If carbonDioxide less than threshold and no presence in the room => log, notify and set parent space computed value 
+        if(carbonDioxideValue < carbonDioxideThreshold && !presence) {
+            log(`${availableFresh}. Carbon Dioxide: ${carbonDioxideValue}. Presence: ${presence}.`);
+            setSpaceValue(parentSpace.Id, spaceAvailFresh, availableFresh);
 
             // Set up custom notification for air quality
-            parentSpace.Notify(JSON.stringify("Room not available or air quality is poor.")); 
+            parentSpace.Notify(JSON.stringify(availableFresh)); 
         }
         else {
-            log(`Room is not available or air quality is poor. Carbon Dioxide: ${CarbonDioxideSensor.Value().Value}. Presence: ${presence}.`);
-            setSpaceValue(parentSpace.Id, spaceAirQuality, "Room is not available or air quality is poor.");
+            log(`${noAvailableOrFresh}. Carbon Dioxide: ${carbonDioxideValue}. Presence: ${presence}.`);
+            setSpaceValue(parentSpace.Id, spaceAvailFresh, noAvailableOrFresh);
 
             // Set up custom notification for air quality
-            parentSpace.Notify(JSON.stringify("Room not available or air quality is poor.")); 
+            parentSpace.Notify(JSON.stringify(noAvailableOrFresh)); 
         }
     }
     catch (error)
@@ -67,11 +70,10 @@ function process(telemetry, executionContext) {
     }
 }
 
-
 function getFloatValue(str) {
   if(!str) {
       return null;
   }
 
   return parseFloat(str);
-}  
+}
